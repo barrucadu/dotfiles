b5f63c9dfbf5cd651c7df8f8dc021dd429b929a9
Barrucadu <mike@barrucadu.co.uk>
2009-05-17 13:35:02 +0100

    Edited bookmarks script to sort bookmarks

--- .config/uzbl/scripts/load_url_from_bookmarks.sh
+++ .config/uzbl/scripts/load_url_from_bookmarks.sh
@@ -4,6 +4,6 @@ bookmarks_file=$XDG_DATA_HOME/uzbl/bookmarks
 COLORS=" -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
 OPTIONS=" -i -xs -rs -l 10"
 
-goto=`< $bookmarks_file dmenu $OPTIONS $COLORS | cut -d ' ' -f -100  | awk '{print $NF}'`
+goto=`sort $bookmarks_file | dmenu $OPTIONS $COLORS | cut -d ' ' -f -100  | awk '{print $NF}'`
 
 [ -n "$goto" ] && uzblctrl -s $5 -c "act uri $goto"
