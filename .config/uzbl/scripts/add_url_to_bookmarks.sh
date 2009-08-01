fd695b6076d2551339f9196747b525d8b8c7dabe
Barrucadu <mike@barrucadu.co.uk>
2009-08-01 22:37:00 +0100

    Tidied up uzbl config and zsh function indenting. Started Project Euler in C.

--- .config/uzbl/scripts/add_url_to_bookmarks.sh
+++ .config/uzbl/scripts/add_url_to_bookmarks.sh
@@ -3,7 +3,7 @@ bookmarks_file=$XDG_DATA_HOME/uzbl/bookmarks
 bookmarks_tags=$XDG_DATA_HOME/uzbl/bookmarks.tags
 
 COLORS=" -nb #303030 -nf khaki -sb #CCFFAA -sf #303030"
-OPTIONS=" -i -xs -rs -l 10"
+OPTIONS=" -i -l 10"
 
 tag=`< $bookmarks_tags dmenu $OPTIONS $COLORS`
 
