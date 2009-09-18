86dfdff0ce50b62e96bb5cc74dbbf03b960999e4
Barrucadu <mike@barrucadu.co.uk>
2009-09-18 23:51:31 +0100

    Began using Zathura; copied config.h to ~/.config for ease-of-recompile. Altered my LaTeX wizard slightly. Xorg/emacs stuff.

---
+++ .config/zathura/config.h
@@ -0,0 +1,79 @@
+/* settings */
+static const float ZOOM_STEP         = 0.1;
+static const float SCROLL_STEP       = 40;
+static const float HL_TRANSPARENCY   = 0.4;
+static const int   SHOW_NOTIFICATION = 5;
+static const int   DEFAULT_WIDTH     = 1280;
+static const int   DEFAULT_HEIGHT    = 800;
+static const char  BROWSER[]         = "opera %s";
+static const char  PRINTER[]         = "";
+
+/* look */
+static const char font[]                  = "onuava normal 10";
+static const char default_bgcolor[]       = "#1C1C1C";
+static const char default_fgcolor[]       = "#DDEEDD";
+
+static const char notification_fgcolor[]         = "#A3BABF";
+static const char notification_bgcolor[]         = "#5E7175";
+static const char notification_warning_fgcolor[] = "#FFE863";
+static const char notification_warning_bgcolor[] = "#B7416E";
+static const char notification_error_fgcolor[]   = "#F00060";
+static const char notification_error_bgcolor[]   = "#FFE863";
+
+static const char inputbar_bgcolor[]      = "#1C1C1C";
+static const char inputbar_fgcolor[]      = "#DDEEDD";
+
+static const char completion_fgcolor[]    = "#DDEEDD";
+static const char completion_bgcolor[]    = "#1C1C1C";
+static const char completion_hl_fgcolor[] = "#A3BABF";
+static const char completion_hl_bgcolor[] = "#5E7175";
+
+static const char search_highlight[] = "#FEA63C";
+
+/* additional settings */
+#define SHOW_SCROLLBARS    0
+#define INCREMENTAL_SEARCH 0
+
+/* shortcuts */
+Shortcut shortcuts[] = {
+  // mask,               key,           function,            argument
+  {GDK_CONTROL_MASK,     GDK_f,         sc_navigate,         { NEXT } },
+  {GDK_CONTROL_MASK,     GDK_b,         sc_navigate,         { PREVIOUS } },
+  {GDK_CONTROL_MASK,     GDK_plus,      sc_zoom,             { ZOOM_IN } },
+  {GDK_CONTROL_MASK,     GDK_minus,     sc_zoom,             { ZOOM_OUT } },
+  {GDK_CONTROL_MASK,     GDK_0,         sc_zoom,             { ZOOM_ORIGINAL } },
+  {GDK_CONTROL_MASK,     GDK_r,         sc_rotate,           { RIGHT } },
+  {GDK_CONTROL_MASK,     GDK_e,         sc_rotate,           { LEFT } },
+  {GDK_CONTROL_MASK,     GDK_q,         sc_quit,             {0} },
+  {0,                    GDK_n,         sc_search,           { FORWARD } },
+  {0,                    GDK_N,         sc_search,           { BACKWARD } },
+  {0,                    GDK_h,         sc_scroll,           { LEFT } },
+  {0,                    GDK_k,         sc_scroll,           { DOWN } },
+  {0,                    GDK_j,         sc_scroll,           { UP } },
+  {0,                    GDK_l,         sc_scroll,           { RIGHT } },
+  {0,                    GDK_Page_Up,   sc_scroll,           { TOP } },
+  {0,                    GDK_Page_Down, sc_scroll,           { BOTTOM } },
+  {0,                    GDK_i,         sc_adjust_window,    { ADJUST_BESTFIT } },
+  {0,                    GDK_u,         sc_adjust_window,    { ADJUST_WIDTH } },
+  {0,                    GDK_colon,     sc_focus_inputbar,   { .data = ":" } },
+  {0,                    GDK_o,         sc_focus_inputbar,   { .data = ":open " } },
+  {0,                    GDK_r,         sc_focus_inputbar,   { .data = ":rotate " } },
+  {0,                    GDK_z,         sc_focus_inputbar,   { .data = ":zoom " } },
+  {0,                    GDK_g,         sc_focus_inputbar,   { .data = ":goto " } },
+  {0,                    GDK_slash,     sc_focus_inputbar,   { .data = "/" } },
+};
+
+/* commands */
+Command commands[] = {
+  // command,         function
+  {"export",          cmd_export},
+  {"form",            cmd_form},
+  {"goto",            cmd_goto},
+  {"info",            cmd_info},
+  {"links",           cmd_links},
+  {"open",            cmd_open},
+  {"rotate",          cmd_rotate},
+  {"save",            cmd_save},
+  {"quit",            cmd_quit},
+  {"zoom",            cmd_zoom},
+};
