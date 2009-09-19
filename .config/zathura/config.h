3b7182a56c69a3d7caab294354eeaf7227a21d1a
Barrucadu <mike@barrucadu.co.uk>
2009-09-19 11:59:37 +0100

    Changed Zathura shortcuts.

--- .config/zathura/config.h
+++ .config/zathura/config.h
@@ -37,28 +37,34 @@ static const char search_highlight[] = "#FEA63C";
 /* shortcuts */
 Shortcut shortcuts[] = {
   // mask,               key,           function,            argument
-  {GDK_CONTROL_MASK,     GDK_f,         sc_navigate,         { NEXT } },
-  {GDK_CONTROL_MASK,     GDK_b,         sc_navigate,         { PREVIOUS } },
-  {GDK_CONTROL_MASK,     GDK_plus,      sc_zoom,             { ZOOM_IN } },
-  {GDK_CONTROL_MASK,     GDK_minus,     sc_zoom,             { ZOOM_OUT } },
-  {GDK_CONTROL_MASK,     GDK_0,         sc_zoom,             { ZOOM_ORIGINAL } },
-  {GDK_CONTROL_MASK,     GDK_r,         sc_rotate,           { RIGHT } },
-  {GDK_CONTROL_MASK,     GDK_e,         sc_rotate,           { LEFT } },
-  {GDK_CONTROL_MASK,     GDK_q,         sc_quit,             {0} },
-  {0,                    GDK_n,         sc_search,           { FORWARD } },
-  {0,                    GDK_N,         sc_search,           { BACKWARD } },
+
+  // Navigation
+  {0,                    GDK_f,         sc_navigate,         { NEXT } },
+  {0,                    GDK_b,         sc_navigate,         { PREVIOUS } },
   {0,                    GDK_h,         sc_scroll,           { LEFT } },
   {0,                    GDK_k,         sc_scroll,           { DOWN } },
   {0,                    GDK_j,         sc_scroll,           { UP } },
   {0,                    GDK_l,         sc_scroll,           { RIGHT } },
   {0,                    GDK_Page_Up,   sc_scroll,           { TOP } },
   {0,                    GDK_Page_Down, sc_scroll,           { BOTTOM } },
+
+  // Searching
+  {0,                    GDK_n,         sc_search,           { FORWARD } },
+  {0,                    GDK_N,         sc_search,           { BACKWARD } },
+
+  // Display
+  {0,                    GDK_plus,      sc_zoom,             { ZOOM_IN } },
+  {0,                    GDK_minus,     sc_zoom,             { ZOOM_OUT } },
+  {0,                    GDK_0,         sc_zoom,             { ZOOM_ORIGINAL } },
   {0,                    GDK_i,         sc_adjust_window,    { ADJUST_BESTFIT } },
   {0,                    GDK_u,         sc_adjust_window,    { ADJUST_WIDTH } },
+  {0,                    GDK_r,         sc_rotate,           { RIGHT } },
+  {0,                    GDK_e,         sc_rotate,           { LEFT } },
+
+  // Misc
+  {0,                    GDK_q,         sc_quit,             {0} },
   {0,                    GDK_colon,     sc_focus_inputbar,   { .data = ":" } },
   {0,                    GDK_o,         sc_focus_inputbar,   { .data = ":open " } },
-  {0,                    GDK_r,         sc_focus_inputbar,   { .data = ":rotate " } },
-  {0,                    GDK_z,         sc_focus_inputbar,   { .data = ":zoom " } },
   {0,                    GDK_g,         sc_focus_inputbar,   { .data = ":goto " } },
   {0,                    GDK_slash,     sc_focus_inputbar,   { .data = "/" } },
 };
@@ -67,13 +73,23 @@ Shortcut shortcuts[] = {
 Command commands[] = {
   // command,         function
   {"export",          cmd_export},
+  {"e",               cmd_export},
   {"form",            cmd_form},
+  {"f",               cmd_form},
   {"goto",            cmd_goto},
+  {"g",               cmd_goto},
   {"info",            cmd_info},
+  {"i",               cmd_info},
   {"links",           cmd_links},
+  {"l",               cmd_links},
   {"open",            cmd_open},
+  {"o",               cmd_open},
   {"rotate",          cmd_rotate},
+  {"r",               cmd_rotate},
   {"save",            cmd_save},
+  {"s",               cmd_save},
   {"quit",            cmd_quit},
+  {"q",               cmd_quit},
   {"zoom",            cmd_zoom},
+  {"z",               cmd_zoom},
 };
