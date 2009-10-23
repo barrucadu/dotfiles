b0f1285febb7da633b1383f6beb6c49226e17f48
Barrucadu <mike@barrucadu.co.uk>
2009-10-23 14:20:23 +0100

    More Pascal practice…

---
+++ code/pascal/die.pas
@@ -0,0 +1,65 @@
+program die;
+
+Var
+   diestr	    : String;
+   a, i, err	    : Integer;
+   looping	    : Boolean;
+   rolls, size, sum : Integer;
+   mean		    : Real;
+
+begin
+   randomize;
+   
+   write('Enter the die string (eg: 1d6): ');
+   readln(diestr);
+
+   a       := 1;
+   i       := 1;
+   rolls   := -1;
+   size    := -1;
+   looping := true;
+   
+   while looping do
+   begin
+      i := i + 1;
+      if rolls = -1 then
+      begin
+	 if diestr[i] = 'd' then
+	    begin
+	       val(copy(diestr, a, i - a), rolls, err);
+	       if err <> 0 then
+	       begin
+		  writeln('An error occurred at ', err, ' (rolls)');
+		  looping := false
+	       end
+	       else
+	       begin
+		  val(copy(diestr, i + 1, Length(diestr)), size, err);
+		  if err <> 0 then
+		  begin
+		     writeln('An error occurred at ', err, ' (size)');
+		     looping := false
+		  end
+		  else
+		     looping := false;
+	       end;
+	    end;
+      end;
+   end;
+
+   if err = 0 then
+   begin
+      sum := 0;
+      
+      for i := 1 to rolls do
+      begin
+	 a := random(size) + 1;
+	 writeln('Roll ', i, ' = ', a);
+	 sum := sum + a;
+      end;
+      
+      mean := sum / rolls;
+      writeln('Sum:  ', sum);
+      writeln('Mean: ', mean:2:2);
+   end;
+end.
