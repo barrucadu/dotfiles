ffef211ca9a6d796e70034cdadd420612bbcc880
Barrucadu <mike@barrucadu.co.uk>
2009-11-05 23:26:39 +0000

    Updated data files - removed ranges

--- .data/pydmx/snowmachine.py
+++ .data/pydmx/snowmachine.py
@@ -1,7 +1,7 @@
 name = "Antari Silent S-200"
 
 dmx = {"volume"  : {"off" : [0, 0, "No snow"],
-                    "on"  : [5, 0, "Snow", 255]}}
+                    "on"  : [5, 0, "Snow"]}}
 
 channels = ["Volume"]
 
