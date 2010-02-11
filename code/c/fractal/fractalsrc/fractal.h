56c29e439e61f551646620521e735ad7a21d73f5
Michael Walker (Barrucadu) <mike@barrucadu.co.uk>
2010-02-11 18:13:46 +0000

    *Properly* split into mpd-utils and fractalgen repos

--- code/c/fractal/fractalsrc/fractal.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FRACTAL_H
-#define __FRACTAL_H
-
-int  in_mandy_set(cmplx c, int *iteration, float *zmod, fractal_options_t foptions);
-int  in_julia_set(cmplx c, int *iteration, float *zmod, fractal_options_t foptions);
-void fractal_main_loop(fractal_options_t foptions, image_options_t ioptions);
-
-#endif
--- code/c/fractal/fviewersrc/fviewer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FVIEWER_H
-#define __FVIEWER_H
-
-gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer data);
-void destroy(GtkWidget *widget, gpointer data);
-int main(int argc, char *argv[]);
-
-#endif
-
