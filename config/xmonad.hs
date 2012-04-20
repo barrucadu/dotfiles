4ef45e4d4e2ea9bd71de6e3a71426aaa426197cc
Michael Walker <mike@barrucadu.co.uk>
2012-04-20 16:35:30 +0100

    Removed Mod+Enter

--- config/xmonad.hs
+++ config/xmonad.hs
@@ -51,9 +51,6 @@ myDefaultKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
  
-    -- Swap the focused window and the master window
-    , ((modm,               xK_Return), windows W.swapMaster)
- 
     -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
  
