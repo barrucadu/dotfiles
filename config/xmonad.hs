bec4fe96f338dfee04003aee91ca387d52246ed2
Michael Walker <mike@barrucadu.co.uk>
2012-04-23 18:21:09 +0100

    Removed all default bindings and added some extra shortcuts

--- config/xmonad.hs
+++ config/xmonad.hs
@@ -22,68 +22,8 @@ myConfig = defaultConfig
            , keys       = myDefaultKeys
            , layoutHook = myLayoutHook }
 
--- Overriding of the default key-bindings is done in here.
-myDefaultKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
- 
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c     ), kill)
- 
-     -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
- 
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
- 
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
- 
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
- 
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
- 
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
- 
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
- 
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
- 
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
- 
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
- 
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
- 
-    -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
- 
-    -- Increment the number of windows in the master area
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
- 
-    -- Deincrement the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
- 
-    -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    ]
-    ++
-  
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+-- Don't have any of the default keybindings
+myDefaultKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList []
 
 -- My extra keys, not merely overriding or disabling of the defaults.
 myKeymap config = 
@@ -105,6 +45,7 @@ myKeymap config =
      ("<XF86AudioRaiseVolume>", spawn "amixer set Master 1%+"),
 
 -- Misc keys
+     ("C-z c",   spawn "urxvt"),
      ("C-z C-c", spawn "urxvt"),
      ("C-\\",    spawn "urxvtc -e newtmux")
     ]
@@ -116,4 +57,15 @@ myKeymap config =
      | (tag, key) <- zip (workspaces' config) "123456789"
      , (otherModMasks, action) <- [ ("",   W.greedyView)
                                  , ("S-", W.shift)]
+    ]
+    ++
+    [
+     ("C-z C-z", windows W.focusDown),
+     ("C-z k",   kill)
+    ]
+    ++
+
+-- Xmonad misc
+    [
+     ("M-F9", spawn "xmonad --recompile; xmonad --restart")
     ]
