50d398fd4d29d0ce2082c2db93c9474a9e5f7366
Michael Walker <mike@barrucadu.co.uk>
2016-05-10 16:40:49 +0100

    Remove llvm-mode

--- emacs/.emacs.d/init/llvm.el
+++
@@ -1,7 +0,0 @@
-(load-file "~/.emacs.d/init/utils.el")
-
-(setq load-path
-      (cons (expand-file-name "~/.emacs.d/llvm-mode/") load-path))
-
-(with-library llvm-mode
-  (add-to-list 'auto-mode-alist '("/*..ll$" . llvm-mode)))
