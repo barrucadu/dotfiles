76947dca716c762dc917c8dd3d09e4cefeae86cd
Michael Walker <mike@barrucadu.co.uk>
2016-10-12 10:30:02 +0100

    Set vc-follow-symlinks earlier.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,8 +1,15 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Follow symlinks to version-controlled files without asking. This
+;; has to be here, rather than in the configuration.el.org, as that
+;; file is version-controlled: whenever it changes emacs asks for
+;; confirmation before loading it.
+
+(setq vc-follow-symlinks t)
+
+;; Load the rest of the configuration.
 (org-babel-load-file "~/.emacs.d/configuration.el.org")
