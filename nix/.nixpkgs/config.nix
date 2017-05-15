6e240b4c5e18672913de49ef8a7d12271e925e37
Michael Walker <mike@barrucadu.co.uk>
2017-05-15 03:44:47 +0100

    Split proselint package out to a separate file.

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -5,25 +5,6 @@
   packageOverrides = super: let self = super.pkgs; in with self; rec {
     ghc802Packages = super.haskell.packages.ghc802;
 
-    proselint = with pkgs.python35Packages; buildPythonPackage rec {
-      name = "proselint-${version}";
-      version = "0.8.0";
-
-      src = pkgs.fetchurl {
-        url = "mirror://pypi/p/proselint/${name}.tar.gz";
-        sha256 = "08d48494533f178eb7a978cbdf10ddf85ed7fc2eb486ff5e7d0aecfa08e81bbd";
-      };
-
-      buildInputs = [ future six ];
-      propagatedBuildInputs = [ click ];
-
-      doCheck = false;
-
-      meta = {
-        description = "A linter for prose";
-        homepage = "http://proselint.com/";
-        license = pkgs.stdenv.lib.licenses.bsd3;
-      };
-    };
+    proselint = import ./pkgs/proselint.nix { pkgs = self; };
   };
 }
