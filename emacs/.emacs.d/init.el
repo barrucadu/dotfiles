6ba864e5dd579e4a41e0b2507875927414e29986
Michael Walker <mike@barrucadu.co.uk>
2017-05-19 02:39:10 +0100

    Shrink emacs init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,20 +1,10 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-;; Use a non-version-controlled file for customised variables.  I
-;; generally don't use this, but some things like to vomit custom
-;; things in here.
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+;; Stop emacs inserting a call to package-initialise here.
+(setq package--init-file-ensured t)
 
 ;; Follow symlinks to version-controlled files without asking. This
 ;; has to be here, rather than in the configuration.el.org, as that
 ;; file is version-controlled: whenever it changes emacs asks for
 ;; confirmation before loading it.
-
 (setq vc-follow-symlinks t)
 
 ;; Load the rest of the configuration.
