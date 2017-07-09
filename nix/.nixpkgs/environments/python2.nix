ba69a9bab09f92e23b23eb3131e4e3de5631c10d
Michael Walker <mike@barrucadu.co.uk>
2017-07-09 23:22:48 +0100

    Add flake8 to Python environments

--- nix/.nixpkgs/environments/python2.nix
+++ nix/.nixpkgs/environments/python2.nix
@@ -4,6 +4,7 @@
   python2Env = pkgs.stdenv.mkDerivation {
     name = "python2-env";
     buildInputs = with pkgs; [
+      python2Packages.flake8
       python2Packages.virtualenv
     ];
   };
--- nix/.nixpkgs/environments/python3.nix
+++ nix/.nixpkgs/environments/python3.nix
@@ -4,6 +4,7 @@
   python3Env = pkgs.stdenv.mkDerivation {
     name = "python3-env";
     buildInputs = with pkgs; [
+      python3Packages.flake8
       python3Packages.virtualenv
     ];
   };
