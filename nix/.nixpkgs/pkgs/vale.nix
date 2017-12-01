b0cd7881096e4ca6076538fd0a467141ad5bcfd9
Michael Walker <mike@barrucadu.co.uk>
2017-12-01 16:46:59 +0000

    Update vale

--- nix/.nixpkgs/pkgs/vale.nix
+++ nix/.nixpkgs/pkgs/vale.nix
@@ -1,16 +1,16 @@
 { pkgs }:
 with pkgs; buildGoPackage rec {
   name = "vale-unstable-${version}";
-  version = "2017-05-14";
+  version = "2017-09-06";
   owner = "ValeLint";
   repo = "vale";
-  rev = "215834049dc696ba6e1dc76ec14402c91d03b78e";
+  rev = "20ed879678910a8f68f4ace41feb45976893db2c";
 
   goPackagePath = "github.com/${owner}/${repo}";
 
   src = fetchFromGitHub {
     inherit owner repo rev;
-    sha256 = "187x6fnsy458g4my9lh4y5wzd1rg5gnin46icb5m3190j5lakpq0";
+    sha256 = "1xaj0y6v7sv47ihaz6zs5y7dxasiingma6akr1wggzb3h457nj20";
   };
 
   # For some reason this needs to be a separate file.  SAD!
