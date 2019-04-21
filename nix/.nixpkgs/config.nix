8cbba8c6b726160a9e2381400fa9cc1b01824599
Michael Walker <mike@barrucadu.co.uk>
2019-04-21 16:28:33 +0100

    Remove development package overrides

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -3,19 +3,6 @@
   allowUnfree = true;
 
   packageOverrides = super: let self = super.pkgs; in with self; rec {
-    # Development
-    ghc802Packages = super.haskell.packages.ghc802;
-    llvm5Debug = super.llvm_5.override { debugVersion = true; };
-
-    profiledHaskellPackages = self.haskellPackages.override {
-      overrides = self: super: {
-        mkDerivation = args: super.mkDerivation (args // {
-          enableLibraryProfiling = true;
-        });
-      };
-    };
-
-    # Writing
     proselint = import ./pkgs/proselint.nix { pkgs = self; };
     vale = import ./pkgs/vale.nix { pkgs = self; };
   };
