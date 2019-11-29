c0b3d5cd0dc853025ddc6dcd6ad223f0a039ff4e
Michael Walker <mike@barrucadu.co.uk>
2019-11-29 12:27:16 +0000

    Add a `govuk ! alias` command to generate aliases

--- misc/bin/govuk-scripts/bang.sh
+++ misc/bin/govuk-scripts/bang.sh
@@ -4,6 +4,17 @@ has() {
   type "$1" >/dev/null
 }
 
+short_env_name() {
+  env="$1"
+  case $env in
+    "integration")    echo "i";;
+    "staging")        echo "s";;
+    "staging-aws")    echo "sa";;
+    "production")     echo "p";;
+    "production-aws") echo "pa";;
+  esac
+}
+
 COMMAND=$1
 if [[ -z $COMMAND ]]; then
   echo "(govuk !) missing subcommand"
@@ -14,6 +25,32 @@ fi
 shift
 
 case $COMMAND in
+  "alias")
+    echo "alias 'gk'='govuk'"
+    echo "alias 'gka'='govuk aws'"
+    for env in integration staging production; do
+      short_env=$(short_env_name "$env")
+      echo "alias 'gka${short_env}'='govuk aws ${env}'"
+      for cmd in a d; do
+        echo "alias 'gka${short_env}${cmd}'='govuk aws ${env} ${cmd}'"
+      done
+    done
+    echo "alias 'gkd'='govuk docker'"
+    for cmd in b B r R c p '!' '?'; do
+      echo "alias 'gkd${cmd}'='govuk docker ${cmd}'"
+    done
+    for db in e mo my p; do
+      echo "alias 'gkds${db}'='govuk docker sync ${db}'"
+    done
+    echo "alias 'gkr'='govuk remote'"
+    for env in integration staging staging-aws production production-aws; do
+      short_env=$(short_env_name "$env")
+      echo "alias 'gkr${short_env}'='govuk remote ${env}'"
+      for cmd in c n s f; do
+        echo "alias 'gkr${short_env}${cmd}'='govuk remote ${env} ${cmd}'"
+      done
+    done
+  ;;
   "check")
     ret=0
     if ! has docker; then
