ab65e26ea13acdd347bed6122d0f799ec184ba21
Michael Walker <mike@barrucadu.co.uk>
2020-11-18 19:16:22 +0000

    Remove proselint and vale packages
    These are in nixpkgs now.

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,9 +1,3 @@
-{ pkgs }:
 {
   allowUnfree = true;
-
-  packageOverrides = super: let self = super.pkgs; in with self; rec {
-    proselint = import ./pkgs/proselint.nix { pkgs = self; };
-    vale = import ./pkgs/vale.nix { pkgs = self; };
-  };
 }
