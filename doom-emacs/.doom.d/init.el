8088393f077575dc88c27bd75f66d8e35a6abb3b
Michael Walker <mike@barrucadu.co.uk>
2022-11-17 22:13:11 +0000

    Add elixir syntax highlighting in emacs

--- doom-emacs/.doom.d/init.el
+++ doom-emacs/.doom.d/init.el
@@ -124,7 +124,7 @@
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
-       ;;elixir            ; erlang done right
+       elixir              ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp          ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
