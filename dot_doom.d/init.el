5bee4593aba29013eccfd454646ad4a224104d58
Michael Walker <mwalker@gocardless.com>
2024-05-16 14:16:47 +0100

    doom: enable javascript package

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -142,7 +142,7 @@
        ;;idris             ; a language you can depend on
        json                ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex               ; writing papers in Emacs has never been so fun
